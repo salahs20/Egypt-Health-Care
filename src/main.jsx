9d9ada7c378126d68ad7ef85c9d91d2a607698f3
Salah Mohamed <salahmohamedalzeini@gmail.com>
2024-12-06 17:52:17 +0200

    update material3

--- src/main.jsx
+++ src/main.jsx
@@ -6,10 +6,10 @@ import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <ThemeProvider>
+    
       <BrowserRouter>
         <App />
       </BrowserRouter>
-    </ThemeProvider>
+    
   </StrictMode>
 );
